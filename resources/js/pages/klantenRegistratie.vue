fa68c8eb6f0599a2d9fdb8d9d87194dc68a9be28
Oscar <97081587@st.deltion.nl>
2026-02-11 10:39:54 +0100

    begin registratie pagina

--- resources/js/pages/klantenRegistratie.vue
+++ resources/js/pages/klantenRegistratie.vue
@@ -1,17 +1,46 @@
 <script setup>
-    import { ref } from 'vue';
+import { ref } from "vue";
 </script>
 
 <template>
     <div class="w-full h-screen">
-    <div 
-        :style="{ backgroundImage: `url('/img/backgroundImage.jpg')` }" 
-        class="w-full h-screen bg-cover bg-center"
-    >
-        <h1 class="">PawHaven</h1> 
-        <div class="rounded-sm bg-white opacity-22 border-white">
-            <p>aa</p>
-        </div> 
-    </div>
+        <div
+            :style="{ backgroundImage: `url('/img/backgroundImage.jpg')` }"
+            class="w-full h-full bg-cover bg-center"
+        >
+            <h1 class="flex justify-center top-20">PawHaven</h1>
+            <div class="flex justify-center items-center">
+                <div
+                    class="rounded-[41px] border-solid border-white bg-white opacity-22 border-white w-200 h-60 blur-[50]"
+                >
+                    <div class="flex flex-col">
+                        <div>
+                            <p class="text-white">Voornaam</p>
+                            <div class="rounded-full bg-white"></div>
+                        </div>
+                        <div>
+                            <p>Achternaam</p>
+                            <div></div>
+                        </div>
+                        <div>
+                            <p>Email</p>
+                            <div></div>
+                        </div>
+                        <div>
+                            <p>Telefoonnummer</p>
+                            <div></div>
+                        </div>
+                        <div>
+                            <p>Wachtwoord</p>
+                            <div></div>
+                        </div>
+                        <div>
+                            <p>Bevestig Wachtwoord</p>
+                            <div></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
