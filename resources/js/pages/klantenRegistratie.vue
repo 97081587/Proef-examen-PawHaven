b7137867fc2b24d92a444ed22cd8fda6efc70654
Oscar <97081587@st.deltion.nl>
2026-02-11 09:53:57 +0100

    klanten table & achtergrondafbeelding toegevoegd

--- resources/js/pages/klantenRegistratie.vue
+++ resources/js/pages/klantenRegistratie.vue
@@ -1,10 +1,17 @@
 <script setup>
-    // bgimage = 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80';
+    import { ref } from 'vue';
 </script>
 
 <template>
-    <div class="rounded-sm bg-white opacity-22">
-        
+    <div class="w-full h-screen">
+    <div 
+        :style="{ backgroundImage: `url('/img/backgroundImage.jpg')` }" 
+        class="w-full h-screen bg-cover bg-center"
+    >
+        <h1 class="">PawHaven</h1> 
+        <div class="rounded-sm bg-white opacity-22 border-white">
+            <p>aa</p>
+        </div> 
     </div>
-    <h1>PawHaven</h1>
+    </div>
 </template>
