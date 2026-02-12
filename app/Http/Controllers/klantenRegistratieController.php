a0ec591281baa8ad253a2154fe15466d515a0d25
Oscar <97081587@st.deltion.nl>
2026-02-12 09:24:57 +0100

    RegiController zou moeten werken?

--- app/Http/Controllers/klantenRegistratieController.php
+++ app/Http/Controllers/klantenRegistratieController.php
@@ -7,8 +7,29 @@ use Inertia\Response;
 
 class klantenRegistratieController
 {
-	public function index(): Response
-	{
-		return Inertia::render('klantenRegistratie');
-	}
+	public function registreren(Request $request) {
+        $validatedData = $request->validate([
+			'RegiFirstName' => ['required', 'string', 'max:255'],
+			'RegiLastName' => ['required', 'string', 'max:255'],
+			'RegiPhoneNumber' => ['required', 'string', 'max:255'],
+            'RegiEmail' => ['required', 'email', Rule::unique('users', 'email')],
+            'RegiPassword' => ['required', 'min:3']
+        ]);
+
+        $register = new User();
+		$register->firstName = $request['RegiFirstName'];
+		$register->lastName = $request['RegiLastName'];
+		$register->phoneNumber = $request['RegiPhoneNumber'];
+        $register->email = $request['RegiEmail'];
+        $register->password = $request['RegiPassword'];
+        
+        $register->password = bcrypt(request('RegiPassword'));
+        $register->save();
+        
+        Session::put('registratie', $register);
+
+        auth()->login($register);
+
+        return redirect('/');
+    }
 }
