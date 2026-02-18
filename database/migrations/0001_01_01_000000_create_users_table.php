ed0fdb222eb3269abd34832cc82cc31e1d0521aa
Oscar <97081587@st.deltion.nl>
2026-02-18 12:25:48 +0100

    Add customer number field to registration and update related logic

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('firstName');
-            $table->string('lastName');
-            $table->string('phoneNumber');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone_number');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('customerNumber')->unique();
+            $table->string('customer_number')->unique();
             $table->rememberToken();
             $table->timestamps();
         });
