36c7df37a268ac92d5d12b6216994163af7067da
Oscar <97081587@st.deltion.nl>
2026-02-18 11:36:09 +0100

    Fix login route casing for consistency

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,11 @@
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\klantenRegistratieController as KlantenRegistratie;
-use App\Http\Controllers\logInController as logIn;
+use App\Http\Controllers\logInController as LogIn;
 use App\Http\Controllers\klantenHomeController;
 
-Route::get('/LogIn', [LogIn::class, 'index'])->name('logIn');
-Route::post('/LogIn', [LogIn::class, 'login']);
+Route::get('/login', [LogIn::class, 'index'])->name('logIn');
+Route::post('/login', [LogIn::class, 'login']);
 
 Route::get('/registratie', [KlantenRegistratie::class, 'index']);
 Route::post('/registratie', [KlantenRegistratie::class, 'registreren']);
