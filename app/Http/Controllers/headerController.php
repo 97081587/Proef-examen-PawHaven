1780712373916cc1e43b5368dc95e0ae26203f99
Oscar <97081587@st.deltion.nl>
2026-03-10 09:55:09 +0100

    Add headerController with logout functionality and commented klantnummer method

---
+++ app/Http/Controllers/headerController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Routing\Controller as BaseController;
+
+class headerController extends BaseController
+{
+    //uitlog functie
+    public function logout(Request $request)
+    {
+        auth()->logout();
+        $request->session()->invalidate(); // destroys session
+        $request->session()->regenerateToken(); // prevents CSRF reuse
+        return redirect('/login');
+    }
+
+    //klantnummer ophalen en laten zien in header
+    // public function klantnummer(Request $request)
+    // {
+    //     $request->validate([
+    //         'klantnummer' => 'required_without:email',
+    //     ]);
+    //     // $klantnummer = auth()->user()->customer_number;
+    //     // return Inertia::render('header', ['klantnummer' => $klantnummer]);
+
+    //     $user = Auth::user();
+
+    //     return Inertia::render('header', [
+    //         'klantnummer' => $user->customer_number,
+    //     ]);
+    // }
+}
