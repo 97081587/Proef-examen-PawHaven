c6afbdedd5df9a996c786d93d3baccad7f17ad8c
Oscar <97081587@st.deltion.nl>
2026-03-17 12:11:52 +0100

    Refactor account deletion route: uncomment dataHash method in klantenHomeControllerAPI and update route in api.php

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,6 @@ use App\Http\Controllers\Api\klantenHomeControllerAPI;
 Route::post('/register', [KlantenRegistratieControllerAPI::class, 'store']);
 
 //home
-// Route::post('/delete-account', [KlantenHome::class, 'dataHash']);
+Route::post('/delete-account', [klantenHomeControllerAPI::class, 'dataHash']);
 
 //header
