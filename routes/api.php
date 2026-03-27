3abc6faa0262f41b376de17d1997de70e21b2099
Oscar <97081587@st.deltion.nl>
2026-03-27 11:16:41 +0100

    Implement headerControllerAPI with logout functionality

--- routes/api.php
+++ routes/api.php
@@ -6,15 +6,8 @@ use App\Http\Controllers\Api\KlantenRegistratieControllerAPI;
 use App\Http\Controllers\Api\klantenLogInControllerAPI;
 use App\Http\Controllers\Api\klantenHomeControllerAPI;
 
-// Route::get('/test', function () {
-//     return response()->json([
-//         'message' => 'API werkt!'
-//     ]);
-// });
-
 // Route::get('/users', [UserController::class, 'index']);
 // Route::post('/users', [UserController::class, 'store']);
-
 
 //login
 // Route::post('/login', [klantenLogInControllerAPI::class, 'login']);
