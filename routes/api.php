922f694afc4cce8e35d0fd0d51fc46af328042f9
Oscar <97081587@st.deltion.nl>
2026-03-30 10:53:33 +0200

    Comment out unused headerControllerAPI import in api.php

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\KlantenRegistratieControllerAPI;
 // use App\Http\Controllers\Api\klantenLogInControllerAPI;
 // use App\Http\Controllers\Api\klantenHomeControllerAPI;
-use App\Http\Controllers\headerControllerAPI as header;
+// use App\Http\Controllers\headerControllerAPI as header;
 
 //registratie (maakt een session aan)
 Route::post('/register', [KlantenRegistratieControllerAPI::class, 'store']);
