f07c8234c0c5c2d13a03c637187f522304964fa7
Oscar <97081587@st.deltion.nl>
2026-03-31 10:30:03 +0200

    Comment out unused HeaderControllerAPI import in api.php for clarity

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\KlantenRegistratieControllerAPI;
 // use App\Http\Controllers\Api\klantenLogInControllerAPI;
 // use App\Http\Controllers\Api\klantenHomeControllerAPI;
-// use App\Http\Controllers\headerControllerAPI as header;
+// use App\Http\Controllers\HeaderControllerAPI;
 
 //registratie (maakt een session aan)
 Route::post('/register', [KlantenRegistratieControllerAPI::class, 'store']);
+
+
+// Route::post('/login', [klantenLogInControllerAPI::class, 'login']);
+
+// Route::post('/delete-account', [klantenHomeControllerAPI::class, 'anonymize'])
+//     ->middleware('auth');
+
+// //header
+// Route::post('/logout', [HeaderControllerAPI::class, 'logout'])->name('logout');
