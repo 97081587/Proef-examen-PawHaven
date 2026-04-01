9dc30d9d122c2245e6c4b21df30d417234cb3616
Oscar <97081587@st.deltion.nl>
2026-04-01 12:05:30 +0200

    Uncomment API route definitions in api.php and comment them in web.php for clearer route management

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,18 @@
 <?php
 
-// use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route;
 // use App\Http\Controllers\Api\UserController;
 
-// use App\Http\Controllers\Api\KlantenRegistratieControllerAPI;
-// use App\Http\Controllers\Api\KlantenLogInControllerAPI;
-// use App\Http\Controllers\Api\KlantenHomeControllerAPI;
-// use App\Http\Controllers\Api\HeaderControllerAPI;
+use App\Http\Controllers\Api\KlantenRegistratieControllerAPI;
+use App\Http\Controllers\Api\KlantenLogInControllerAPI;
+use App\Http\Controllers\Api\KlantenHomeControllerAPI;
+use App\Http\Controllers\Api\HeaderControllerAPI;
 
-// Route::post('/register', [KlantenRegistratieControllerAPI::class, 'store']);
+Route::post('/register', [KlantenRegistratieControllerAPI::class, 'store']);
 
-// Route::post('/login', [KlantenLogInControllerAPI::class, 'login']);
+Route::post('/login', [KlantenLogInControllerAPI::class, 'login']);
 
-// Route::post('/delete-account', [KlantenHomeControllerAPI::class, 'anonymize'])
-//     ->middleware('auth');
+Route::post('/delete-account', [KlantenHomeControllerAPI::class, 'anonymize'])
+    ->middleware('auth');
 
-// Route::post('/logout', [HeaderControllerAPI::class, 'logout'])->name('logout');
+Route::post('/logout', [HeaderControllerAPI::class, 'logout'])->name('logout');
