cb2368582f4fc629de664841b50e7dac149ee500
Oscar <97081587@st.deltion.nl>
2026-04-02 09:51:59 +0200

    Reorganize API route definitions in api.php and web.php for improved clarity and management

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KlantenRegistratieController;
 use App\Http\Controllers\LogInController;
 use App\Http\Controllers\KlantenHomeController;
-// api controllers
-// use App\Http\Controllers\Api\HeaderControllerAPI;
-// use App\Http\Controllers\Api\KlantenLogInControllerAPI;
-// use App\Http\Controllers\Api\KlantenHomeControllerAPI;
-// use App\Http\Controllers\Api\KlantenRegistratieControllerAPI;
 
 //login
 Route::get('/login', [LogInController::class, 'index'])->name('login');
@@ -19,13 +14,3 @@ Route::get('/registratie', [KlantenRegistratieController::class, 'index']);
 
 //home
 Route::get('/', [KlantenHomeController::class, 'index'])->middleware('auth');
-
-//api controllers zitten hier voor de ingelogde session
-// Route::post('/register', [KlantenRegistratieControllerAPI::class, 'store']);
-
-// Route::post('/login', [KlantenLogInControllerAPI::class, 'login']);
-
-// Route::post('/delete-account', [KlantenHomeControllerAPI::class, 'anonymize'])
-//     ->middleware('auth');
-
-// Route::post('/logout', [HeaderControllerAPI::class, 'logout'])->name('logout');
