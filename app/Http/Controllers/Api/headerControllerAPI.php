00b9b54f62d4cbd99726c74524782484f57f7f83
Oscar <97081587@st.deltion.nl>
2026-04-08 10:51:51 +0200

    Refactor KlantenRegistratieControllerAPI to include success message in response and clean up commented token code

--- app/Http/Controllers/Api/headerControllerAPI.php
+++ app/Http/Controllers/Api/headerControllerAPI.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use Inertia\Inertia;
-use Illuminate\Support\Facades\Auth;
+// use Illuminate\Support\Facades\Auth;
 use Illuminate\Routing\Controller as BaseController;
 
 class HeaderControllerAPI extends BaseController
