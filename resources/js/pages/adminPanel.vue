f59229bee97a41f57e8219ecdc47a852b20fb064
Oscar <97081587@st.deltion.nl>
2026-04-13 12:09:13 +0200

    Fix: add empty content div in admin panel template

--- resources/js/pages/adminPanel.vue
+++ resources/js/pages/adminPanel.vue
@@ -14,6 +14,7 @@ import HeaderAdmin from "../layouts/headerAdmin.vue";
         <!-- Content-->
         <div class="relative z-10 h-full flex flex-col text-white">
             <HeaderAdmin />
+            <div class=""></div>
         </div>
     </div>
 </template>
