f828104759b01d60875f45f850c323fb06b62869
Oscar <97081587@st.deltion.nl>
2026-05-06 14:34:08 +0200

    Fix: implement layout for klantenAdminComp component in admin panel

--- resources/js/components/klantenAdminComp.vue
+++ resources/js/components/klantenAdminComp.vue
@@ -1,6 +1,16 @@
 <script setup></script>
 
 <template>
-    <div class=""></div>
+    <div class="flex flex-row justify-between px-30 py-5">
+        <p>Voornaam</p>
+        <p>Achternaam</p>
+        <p>E-mail</p>
+        <p>Telefoonnummer</p>
+        <div class="flex space-x-2">
+            <p>Acties</p>
+            <p>Acties</p>
+            <p>Acties</p>
+        </div>
+    </div>
     <hr class="border-t border-white/100" />
 </template>
