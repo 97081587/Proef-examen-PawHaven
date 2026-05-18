bcb2128145963c90711eeb880198f0e7c23116db
Oscar <97081587@st.deltion.nl>
2026-05-18 12:17:07 +0200

    Fix: correct user retrieval method and update anonymization placeholders in destroy function

--- app/Http/Controllers/Api/adminKlantenDeleteControllerAPI.php
+++ app/Http/Controllers/Api/adminKlantenDeleteControllerAPI.php
@@ -12,12 +12,12 @@ class adminKlantenDeleteControllerAPI extends Controller
         // alle data wordt geanonimiseerd
     public function destroy(Request $request, $id)
     {
-        $user = UserfindOrFail($id);
+        $user = User::findOrFail($id);
 
         $user->update([
-            'first_name' => 'deleted',
-            'last_name' => 'deleted',
-            'phone_number' => 'deleted',
+            'first_name' => 'deleted_user',
+            'last_name' => 'deleted_user',
+            'phone_number' => 'deleted_user',
             'email' => 'deleted_' . $user->id . '@anon.local', // unique email to prevent conflicts
             'customer_number' => 'anon_' . $user->id,
             // 'password' => bcrypt($user->password), // optional, already hashed
