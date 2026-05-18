2027227d51009a086191937dadd2cb153b4a71fc
Oscar <97081587@st.deltion.nl>
2026-05-18 10:34:18 +0200

    Fix: update template structure in klantenAdminComp.vue for proper HTML semantics

--- resources/js/components/klantenAdminComp.vue
+++ resources/js/components/klantenAdminComp.vue
@@ -28,11 +28,11 @@ const deleteAccount = async () => {
 </script>
 
 <template>
-    <div class="flex flex-row justify-between px-30 py-5">
-        <p>{{ first_name }}</p>
-        <p>{{ last_name }}</p>
-        <p>{{ email }}</p>
-        <p>{{ phone_number }}</p>
+    <td class="flex flex-row justify-between px-30 py-5">
+        <tr>{{ first_name }}</tr>
+        <tr>{{ last_name }}</tr>
+        <tr>{{ email }}</tr>
+        <tr>{{ phone_number }}</tr>
         <div class="flex space-x-2">
             <button
                 class="rounded-[41px] border border-white bg-white/20 text-xs"
@@ -50,6 +50,6 @@ const deleteAccount = async () => {
                 Klantvoorkeuren bekijken
             </button>
         </div>
-    </div>
+    </td>
     <hr class="border-t border-white/100" />
 </template>
