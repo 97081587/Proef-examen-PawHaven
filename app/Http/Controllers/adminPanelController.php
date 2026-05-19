efcab9ee269e9ecd1ada877d43313456d9b06e1b
Oscar <97081587@st.deltion.nl>
2026-05-19 11:01:34 +0200

    Fix: add 'id' to user data retrieval in fetchAllIndex and update adminPanel.vue to correctly pass user props to KlantenAdminComp

--- app/Http/Controllers/adminPanelController.php
+++ app/Http/Controllers/adminPanelController.php
@@ -11,6 +11,7 @@ class AdminPanelController extends BaseController
     {
         return Inertia::render('adminPanel', [
             'users' => \App\Models\User::query()->get([
+                'id',
                 'first_name',
                 'last_name',
                 'phone_number',
