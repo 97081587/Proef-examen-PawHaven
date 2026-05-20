f53badc5d44b8bfec9bd9534e4cd1157b61bc1b3
Oscar <97081587@st.deltion.nl>
2026-05-20 09:22:59 +0200

    Feat: add huisdierComp component and integrate it into klantenHuisdieren page

--- resources/js/pages/klantenHuisdieren.vue
+++ resources/js/pages/klantenHuisdieren.vue
@@ -1,9 +1,13 @@
 <script setup>
 import Header from "../layouts/header.vue";
+import huisdierComp from "../components/huisdierComp.vue";
+defineProps({
+    huisdieren: Array,
+});
 </script>
 
 <template>
-        <div
+    <div
         class="w-full h-screen bg-cover bg-center relative"
         style="background-image: url(&quot;/img/backgroundImage.jpg&quot;)"
     >
@@ -14,6 +18,20 @@ import Header from "../layouts/header.vue";
         <div class="relative z-10 h-full flex flex-col text-white">
             <!-- Header -->
             <Header />
-    </div>
+            <div class="flex justify-center top-20 relative">
+                <div
+                    class="absolute z-10 rounded-[41px] border border-white bg-white/20 w-[1200px] h-[547px] backdrop-blur-[30px]"
+                >
+                    <huisdierComp
+                        v-for="huisdier in huisdieren"
+                        :key="huisdier.id"
+                        :name="huisdier.name"
+                        :species="huisdier.species"
+                        :breed="huisdier.breed"
+                        :age="huisdier.age"
+                    />
+                </div>
+            </div>
+        </div>
     </div>
 </template>
