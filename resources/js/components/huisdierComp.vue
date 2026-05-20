8d4f77e649a392a236423e23c2ca7350c8aa7dec
Oscar <97081587@st.deltion.nl>
2026-05-20 10:50:54 +0200

    Feat: enhance huisdierComp with props and add edit button; create pets migration file

--- resources/js/components/huisdierComp.vue
+++ resources/js/components/huisdierComp.vue
@@ -1,8 +1,18 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+
+const props = defineProps({
+    name: String,
+    species: String,
+    breed: String,
+    age: Number,
+});
+</script>
 
 <template>
     <div
-        class="absolute z-10 rounded-[41px] border border-white bg-white/20 w-[1200px] h-[547px] backdrop-blur-[30px]"
+        class="absolute rounded-[41px] border border-white bg-white/20 w-[12px] h-[57px] backdrop-blur-[30px]"
     >
         <div class="flex flex-col items-center justify-center h-full text-white">
             <h2 class="text-2xl font-bold mb-4">Huisdiergegevens</h2>
@@ -10,6 +20,9 @@
              <p class="text-lg mb-2">Leeftijd: {{ age }}</p>
             <p class="text-lg mb-2">Soort: {{ species }}</p>
             <p class="text-lg mb-2">Ras: {{ breed }}</p>
+            <button class="mt-4 rounded-[41px] border border-white bg-white/20 w-[100px] h-[40px]">
+                Bewerken
+            </button>
         </div>
     </div>
 </template>
