52307b7b608a6ed7e2476b176c99c116bfc5c872
Oscar <97081587@st.deltion.nl>
2026-05-21 08:45:39 +0200

    Feat: add klantenHuisdierenRegistratie method and route; implement klantenHuisdierenForm component

---
+++ resources/js/pages/klantenHuisdierenForm.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Header from "../layouts/header.vue";
+</script>
+
+<template>
+    <div
+        class="w-full h-screen bg-cover bg-center relative"
+        style="background-image: url(&quot;/img/backgroundImage.jpg&quot;)"
+    >
+        <!--Transparent layer-->
+        <div class="absolute inset-0 bg-black/25"></div>
+
+        <!--Content-->
+        <div class="relative z-10 h-full flex flex-col text-white">
+            <!-- Header -->
+            <Header />
+            <div class="flex justify-center top-20 relative">
+                <div
+                    class="absolute z-10 rounded-[41px] border border-white bg-white/20 w-[1200px] h-[547px] backdrop-blur-[30px]"
+                >
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
